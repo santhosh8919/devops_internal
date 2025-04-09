2e73d70c2bdea3b3cdadb412044bc253cc74ff6c
SANTHOSH MUDAVATH <157015703+santhosh8919@users.noreply.github.com>
2025-04-09 19:28:36 +0530

    Update hello_updated.java

--- hello_updated.java
+++ hello_updated.java
@@ -1,7 +1,7 @@
 public class hello_updated {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World! Updated!");
+        System.out.println("Hello, World! Updated! 22");
         System.out.println("This is a new line added to the code.");
         System.out.println("Another update to the code.");
         System.out.println("Final update to the code.");
